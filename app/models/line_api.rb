47872d05c840622142f2238bb3c8dc7727305735
mugitti9 <mugikurash@gmail.com>
2021-09-08 13:26:08 +0900

    add_new_user

--- app/models/line_api.rb
+++ app/models/line_api.rb
@@ -1,6 +1,6 @@
 require 'line/bot'
 
-class LineApi < ApplicationRecord
+class LineApi
   def self.get_user_name(user_id)
     uri = URI.parse("https://api.line.me/v2/bot/profile/" + user_id)
     req = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
@@ -12,10 +12,10 @@ class LineApi < ApplicationRecord
     https.verify_mode = OpenSSL::SSL::VERIFY_NONE
     res = https.request(req)
 
-    JSON.parse(res.body)['displayName']
+    JSON.parse(res.body)
   end
 
-  def self.message_push(user_id, message)
+  def self.push_message(user_id, message)
     client = Line::Bot::Client.new { |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_CHANNEL_TOKEN']
