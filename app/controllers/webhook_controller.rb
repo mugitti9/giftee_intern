306b8e71803cdf1180ff6feb516e6ecb2e2f4c60
mugitti9 <mugikurash@gmail.com>
2021-09-08 20:10:58 +0900

    会員登録終了後にチケットを発行

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -24,16 +24,18 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Follow
         line_user_id = event['source']['userId']
         if !Customer.exists?(line_user_id: line_user_id) && line_user_id.present?
-          name = LineApi.get_profile(line_user_id)['displayName']
-          customer = Customer.create!(name: name, line_user_id: line_user_id)
+          ActiveRecord::Base.transaction do
+            name = LineApi.get_profile(line_user_id)['displayName']
+            customer = Customer.create!(name: name, line_user_id: line_user_id)
 
-          # 登録完了後のチケットを送る
-          item = Item.find_by(detail_use: "初回登録")
-          tickets = GajoenApi.get_tickets(item.brand_id, item.item_id)
-          message = "登録ありがとうございました！チケットはこちらになります\n" + tickets['url']
-          LineApi.push_message(line_user_id, message)
-          tickets['request_code']
-          ticket = Ticket.create!(create_ticket_prams(tickets, customer.id))
+            # 登録完了後のチケットを送る
+            item = Item.find_by(detail_use: "初回登録")
+            request_code = SecureRandom.urlsafe_base64(30)
+            tickets = GajoenApi.issue_tickets(item.brand_id, item.item_id, request_code)
+            ticket = Ticket.create!(create_ticket_prams(tickets, customer.id, request_code))
+            message = "登録ありがとうございました！チケットはこちらになります\n" + tickets['url']
+            LineApi.push_message(line_user_id, message)
+          end
 
           message = "会員登録をすると更にチケットが貰えます！\n" + "会員登録をするためには、「会員登録　生年月日 性別」(例：会員登録　1999/1/27 男)と入力してください" 
           LineApi.push_message(line_user_id, message)
@@ -47,29 +49,28 @@ class WebhookController < ApplicationController
               customer = Customer.find_by(line_user_id: event['source']['userId'])
               profile = customer.profile
               if profile.nil?
-                text_list = input_text.split(/[[:blank:]]+/)
-                birthday = Time.parse(NKF.nkf('-m0Z1 -w', text_list[1]))
-                sex = text_list[2]
-                
-                Profile.create!(birthday: birthday, sex: sex, customer_id: customer.id)
+                ActiveRecord::Base.transaction do
+                  text_list = input_text.split(/[[:blank:]]+/)
+                  birthday = Time.zone.parse(NKF.nkf('-m0Z1 -w', text_list[1]))
+                  sex = text_list[2]
+                  
+                  Profile.create!(birthday: birthday, sex: sex, customer_id: customer.id)
 
-                item = Item.find_by(detail_use: "初回登録")
-                tickets = GajoenApi.get_tickets(item.brand_id, item.item_id)
-                output_message = "会員登録ありがとうございました！チケットはこちらになります\n" + tickets['url']
-                LineApi.push_message(line_user_id, message)
-                ticket = Ticket.create!(create_ticket_prams(tickets, customer.id))
+                  item = Item.find_by(detail_use: "初回登録")
+                  request_code = SecureRandom.urlsafe_base64(30)
+                  tickets = GajoenApi.issue_tickets(item.brand_id, item.item_id, request_code)
+                  LineApi.push_message(line_user_id, message)
+                  ticket = Ticket.create!(create_ticket_prams(tickets, customer.id, request_code))
+                  output_message = "会員登録ありがとうございました！チケットはこちらになります\n" + tickets['url']
+                  LineApi.reply_message(event['replyToken'], output_message)
+                end  
               else
-                output_message = "すでに会員登録済です"  
+                output_message = "すでに会員登録済です"
               end
             rescue
               output_message = "適切な文章を入力して下さい"
             end
-
-            message = {
-              type: 'text',
-              text: output_message
-            }
-            client.reply_message(event['replyToken'], message)
+            LineApi.reply_message(event['replyToken'], output_message)
           end
         end
       end
@@ -79,16 +80,15 @@ class WebhookController < ApplicationController
 
   private
 
-  def create_ticket_prams(params, id)
+  def create_ticket_prams(params, id, request_code)
     {
-      :ticket_code      =>  params['code'],
-      :request_code     =>  params['request_code'],
-      :url              =>  params['url'],
-      :status           =>  "issued",
-
-      :available_begin  =>  params['activatable_begin'],
-      :available_end    =>  params['activatable_end'],
-      :customer_id      =>  id,
+      ticket_code:      params['code'],
+      request_code:     request_code,
+      url:              params['url'],
+      status:           params['status'],
+      available_begin:  Time.zone.at(params['available_begin']),
+      available_end:    Time.zone.at(params['available_end']),
+      customer_id:      id,
     }
   end
 end
